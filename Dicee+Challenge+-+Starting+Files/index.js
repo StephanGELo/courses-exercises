b180e38ce3446e959282cfded5bc6202b480a700
Stephan Lefort <lefortsge@gmail.com>
2024-03-26 23:28:40 -0700

    Complete challenge on dice

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,8 +1,22 @@
-var randomNumber1 = Math.random();
-randomNumber1 = Math.floor(randomNumber1 * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var attribute = document.getElementsByClassName("img1")[0].attributes.src;
+var attribute1 = "./images/dice" + randomNumber1 + ".png";
+const img1 = document.getElementsByClassName("img1")[0];
+img1.setAttribute("src",attribute1);
 
-attribute = JSON.stringify("./images/dice"+randomNumber1+".png");
-console.log(attribute);
-document.getElementsByClassName("img1")[0].attributes.src=attribute;
+var attribute2 = "./images/dice" + randomNumber2 + ".png";
+const img2 = document.getElementsByClassName("img2")[0];
+img2.setAttribute("src", attribute2);
+
+var h1 = document.getElementsByTagName("h1")[0];
+
+if (randomNumber1 > randomNumber2) {
+    h1.innerHTML = " 🚩 Play 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2) {
+    h1.innerHTML = "Player 2 Wins! 🚩";
+} 
+else {
+    h1.innerHTML = "Draw!";
+}
