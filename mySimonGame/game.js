95135d1bba258c9faa6025e081bb61143805eee9
Stephan Lefort <lefortsge@gmail.com>
2024-03-31 11:42:54 -0700

    Add sound when a button is clicked and create the pattern of the buttons the user clicked

--- mySimonGame/game.js
+++ mySimonGame/game.js
@@ -1,5 +1,12 @@
 var buttonColors = ['red', 'blue', 'green', 'yellow'];
 var gamePattern = [];
+var userClickedPattern = [];
+
+function playSound(name) {
+  //Play the audio corresponding to the randomChosenColor
+  var audio = new Audio('./sounds/' + name + '.mp3');
+  audio.play();
+}
 
 function nextSequence() {
   // generate a random number from 0 to 3
@@ -11,7 +18,12 @@ function nextSequence() {
   var id = "#" + randomChosenColor;
   //Flash the button of the randomChosenColor
   $(id).fadeIn(100).fadeOut(100).fadeIn(100);
-  //Play the audio corresponding to the randomChosenColor
-  var audio = new Audio('./sounds/' + randomChosenColor + '.mp3');
-  audio.play();
+  playSound(randomChosenColor);
 }
+
+//Create user clicked pattern
+$('.btn').click(function(){
+  var userChosenColor = $(this).attr("id");
+  playSound(userChosenColor);
+  userClickedPattern.push(userChosenColor);
+});
