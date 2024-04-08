7da90d7eef52cafb6a22a842f317e18ffdb446a3
Stephan Lefort <lefortsge@gmail.com>
2024-04-08 13:30:15 -0700

    Create a middleware to log the method and url of the request using ExpressJs use() method

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -3,6 +3,12 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger (req, res, next) {
+  console.log("The Request Method: ", req.method);
+  console.log("The Request URL: ", req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
