7149a37d6a1a6966edaf00c35de7a824ae6b12a4
Stephan Lefort <lefortsge@gmail.com>
2024-04-12 21:34:37 -0700

    Pass data to EJS templates

---
+++ Backend/4.2+Passing+Data/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  var fullName = req.body.fName + req.body.lName;
+  var numberOfLetters = fullName.length;
+  res.render("index.ejs", { lettersCount : numberOfLetters });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
