6998890a46f67108f3c7f19b01b247916f3046c5
Stephan Lefort <lefortsge@gmail.com>
2024-04-16 21:54:00 -0700

    Add static files and css to the application

--- Backend/4.4 Band Generator Project/index.js
+++ Backend/4.4 Band Generator Project/index.js
@@ -8,6 +8,7 @@ const port = 3000;
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
+app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
