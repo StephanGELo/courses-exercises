0db3c635a4b8dea0bc5c7fa4df5cc5aae8c79efb
Stephan Lefort <lefortsge@gmail.com>
2024-04-22 13:10:39 -0700

    Initialise project with NPM packages, Set up Node.js server and create skeleton and render ejs file

---
+++ Backend/CP_Construction Tech Blog/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended : true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}...`);
+});
