37b4183086706093933739d8d9dd5cd015b0eec6
Stephan Lefort <lefortsge@gmail.com>
2024-04-23 09:51:46 -0700

    Connect stylesheet to project and test output

--- Backend/CP_Construction Tech Blog/index.js
+++ Backend/CP_Construction Tech Blog/index.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended : true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
