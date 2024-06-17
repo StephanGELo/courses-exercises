4874499f19e26a07b1008ffee57de339bed458d3
Stephan Lefort <lefortsge@gmail.com>
2024-06-17 14:19:22 -0700

    Create sucessfully a Postgres Database named World with a table for capitals and successfully serve the data to the client-side

--- Backend/World+Capital+Quiz/index.js
+++ Backend/World+Capital+Quiz/index.js
@@ -1,14 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Elluel1981$$",
+  port: 5432
+});
+
+db.connect();
 
 let quiz = [
   { country: "France", capital: "Paris" },
   { country: "United Kingdom", capital: "London" },
   { country: "United States of America", capital: "New York" },
 ];
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    quiz = res.rows;
+  }
+  db.end();
+});
+
 
 let totalCorrect = 0;
 
