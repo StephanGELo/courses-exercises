042195851cfd0ae6a143c2ae62d0aeeeeb7d9cf2
Stephan Lefort <lefortsge@gmail.com>
2024-06-19 06:03:19 -0700

    Implement code to connect to Postgres DB and serve the data to the client side to render world map of visited countries

--- Backend/8.3 Travel Tracker/index.js
+++ Backend/8.3 Travel Tracker/index.js
@@ -1,14 +1,33 @@
+import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: `${process.env.DB_DATABASE}`,
+  password: `${process.env.DB_PASSWORD}`,
+  port: 5432,
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   //Write your code here.
+  const result = await db.query("SELECT * FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((row) => {
+    countries.push(row.country_code);
+  });
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
--- Backend/8.3 Travel Tracker/solution1.js
+++ Backend/8.3 Travel Tracker/solution1.js
@@ -9,7 +9,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
-  password: "123456",
+  password: `${process.env.DB_PASSWORD}`,
   port: 5432,
 });
 db.connect();
