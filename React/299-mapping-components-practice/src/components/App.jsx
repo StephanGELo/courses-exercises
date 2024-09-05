c9e0902255e794054d9afe216d098198137e051c
Stephan Lefort <lefortsge@gmail.com>
2024-09-05 12:33:27 -0700

    Refactor for reusability by including an Entry components and using props to pass the data down from the parent component

--- React/299-mapping-components-practice/src/components/App.jsx
+++ React/299-mapping-components-practice/src/components/App.jsx
@@ -1,19 +1,14 @@
 import React from "react";
+import Entry from "./Entry";
 import emojipedia  from "../emojipedia";
 
-function createEmojiCard(emoji) {
+function createEmojiCard(emojiTerm) {
   return (
-    <div className="term">
-      <dt>
-        <span className="emoji" role="img" aria-label="Tense Biceps">
-          {emoji.emoji}
-        </span>
-        <span>{emoji.name}</span>
-      </dt>
-      <dd>
-        {emoji.meaning}
-      </dd>
-    </div>
+    <Entry 
+    emoji={emojiTerm.emoji}
+    name={emojiTerm.name}
+    description={emojiTerm.meaning}
+    />
   );
 };
 
@@ -26,44 +21,6 @@ function App() {
 
       <dl className="dictionary">
        {emojipedia.map(createEmojiCard)}
-        {/* <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div> */}
       </dl>
     </div>
   );
