659ad02b05c82df61f6c66d8f37ca645171a9531
Stephan Lefort <lefortsge@gmail.com>
2024-09-11 15:12:11 -0700

    Use of Ternary and && to render login or register page

--- React/304-conditional-rendering-practice/src/components/App.jsx
+++ React/304-conditional-rendering-practice/src/components/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Form from "./Form";
 
-var userIsRegistered = false;
+var userIsRegistered = true;
 
 function App() {
   return (
     <div className="container">
-      <Form />
+     <Form isRegistered={userIsRegistered}/>
     </div>
   );
 }
