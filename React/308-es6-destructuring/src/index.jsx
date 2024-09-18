b05b7b14023afb6aba3a4863595cd1b2c9a7a83a
Stephan Lefort <lefortsge@gmail.com>
2024-09-18 15:24:31 -0700

    Render car stats by using ES6 data destructuring to obtain the information from the car file

---
+++ React/308-es6-destructuring/src/index.jsx
@@ -0,0 +1,48 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
+import React from "react";
+import ReactDOM from "react-dom";
+import cars from "./practice";
+
+const [ honda, tesla ] = cars;
+console.log(honda);
+const { 
+    speedStats: { topSpeed : hondaTopSpeed }
+} = honda;
+const { 
+    speedStats: { topSpeed : teslaTopSpeed }
+} = tesla;
+
+const {
+    coloursByPopularity: [hondaTopColour]
+} = honda;
+
+const {
+    coloursByPopularity: [teslaTopColour]
+} = tesla;
+
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+      <th>Top Colour</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
