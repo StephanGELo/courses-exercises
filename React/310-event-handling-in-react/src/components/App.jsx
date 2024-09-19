f604cb27796530bd5e92af4eb3fe5950942579ba
Stephan Lefort <lefortsge@gmail.com>
2024-09-18 17:55:09 -0700

    Use event handlers and hooks to change the state of the button and text when a user interact with the page

--- React/310-event-handling-in-react/src/components/App.jsx
+++ React/310-event-handling-in-react/src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [headText, setHeadText] = useState("Hello");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick(){
+    setHeadText("Submitted");
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button 
+        style={{ backgroundColor: isMouseOver ? "black" : "white"}}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+
     </div>
   );
 }
