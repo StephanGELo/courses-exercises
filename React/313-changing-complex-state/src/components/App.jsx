46cf4279e6f384026d154dec596e09d480aa9658
Stephan Lefort <lefortsge@gmail.com>
2024-09-19 12:47:23 -0700

    Use of hooks to handle complex state update for example to render first and last name in a form

--- React/313-changing-complex-state/src/components/App.jsx
+++ React/313-changing-complex-state/src/components/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [ fName, setFName ] = useState("");
+  const [ lName, setLName ] = useState("");
+  
+
+  function updateFName(event) {
+    setFName(event.target.value)
+  }
+
+  function updateLName(event) {
+    setLName(event.target.value);
+  }
+
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>Hello {fName} {lName}</h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input onChange={updateFName} name="fName" placeholder="First Name"  value={fName}/>
+        <input onChange={updateLName} name="lName" placeholder="Last Name" value={lName} />
         <button>Submit</button>
       </form>
     </div>
