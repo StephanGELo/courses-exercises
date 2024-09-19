a8d26da78b92f5cc0729d9e38e412545f3af9dd4
Stephan Lefort <lefortsge@gmail.com>
2024-09-19 09:51:11 -0700

    Update the input state only when button is clicked to display the input text

--- React/311-react-forms/src/components/App.jsx
+++ React/311-react-forms/src/components/App.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [ name, setName ] = useState("");
+  const [ heading, setHeading ] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleSubmit() {
+   setHeading(name);
+  }
 
   return (
     <div className="container">
-      <h1>Hello  </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>Hello {heading} </h1>
+      <input 
+      onChange={handleChange}
+      type="text" 
+      placeholder="What's your name?" 
+      value={name}
+      />
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
