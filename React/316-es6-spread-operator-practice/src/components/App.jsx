d5e7ab195484bccfa9127431fbe16266baf5f8a1
Stephan Lefort <lefortsge@gmail.com>
2024-09-24 11:22:15 -0700

    Use of hooks and ES6 spread operator to handle changing complex state and to render To-Do-List

--- React/316-es6-spread-operator-practice/src/components/App.jsx
+++ React/316-es6-spread-operator-practice/src/components/App.jsx
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [item, setItem ] = useState("");
+  const [ list, setList ] = useState([]);
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setItem(value);
+    event.target.placeholder = "";
+  }
+  
+  function handleClick() {
+    setList(prevList => {
+      return (
+        [...prevList, item]
+      )
+    });  
+    setItem("");  
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={item}/>
+        <button onClick={handleClick}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {
+            list.map((listItem, index) => {
+              return <li key={index} >{listItem}</li>
+            })
+          }
         </ul>
       </div>
     </div>
