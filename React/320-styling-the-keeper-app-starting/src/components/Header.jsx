3deeff5988ea3c4ca0b917f75d993401b26ad77f
Stephan Lefort <lefortsge@gmail.com>
2024-09-30 11:46:56 -0700

    Add a highlightedIcon at the header

--- React/320-styling-the-keeper-app-starting/src/components/Header.jsx
+++ React/320-styling-the-keeper-app-starting/src/components/Header.jsx
@@ -4,7 +4,7 @@ import HighlightIcon from "@mui/icons-material/Highlight";
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1><HighlightIcon />Keeper</h1>
     </header>
   );
 }
